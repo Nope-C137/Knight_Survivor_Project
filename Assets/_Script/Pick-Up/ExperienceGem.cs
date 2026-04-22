e7a3e83ea28cc1a74f08db7afef234f84edd5098
ss23sss <phamcc2@gmail.com>
2026-04-22 10:30:16 +0700

    Updated auto aim, item rotated

--- Assets/_Script/Pick-Up/ExperienceGem.cs
+++ Assets/_Script/Pick-Up/ExperienceGem.cs
@@ -4,12 +4,22 @@ namespace TD
 {
     public class ExperienceGem : MonoBehaviour, ICollectible
     {
+        public float rotateSpeed;
         public int experienceAmountGranted = 10;
+        
+        private void Update()
+        {
+            Handle_Rotation();
+        }
         public void Collect()
         {
             PlayerStats player = FindAnyObjectByType<PlayerStats>();
             player.IncreaseExperience(experienceAmountGranted);
             Destroy(gameObject);
+        }
+        private void Handle_Rotation()
+        {
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
         }
     }
 }
--- Assets/_Script/Pick-Up/HealthPotion.cs
+++ Assets/_Script/Pick-Up/HealthPotion.cs
@@ -1,15 +1,27 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
 
 namespace TD
 {
     public class HealthPotion : MonoBehaviour, ICollectible
     {
+        public float rotateSpeed;
         public int healhRestoreAmount = 50; // Số lượng máu hồi phục khi thu thập bình máu
         public void Collect()
         {
             PlayerStats player = FindFirstObjectByType<PlayerStats>();
             player.ResstoreHealth(healhRestoreAmount);
             Destroy(gameObject); // Hủy đối tượng bình máu sau khi thu thập
+        }
+        
+
+        private void Update()
+        {
+            Handle_Rotation();
+        }
+        private void Handle_Rotation()
+        {
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
         }
     }
 
